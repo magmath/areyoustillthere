e5649ab1a1211b8c7503d5fb4d9032963a141a23
matthewdtwo <matthewlittle2006@gmail.com>
2015-12-17 02:42:39 -0600

    modified script

--- areyoustillthere.sh
+++ areyoustillthere.sh
@@ -104,11 +104,11 @@ function main() {
 			mosquitto_pub -t $MOSQUITTO_TOPIC -h $MOSQUITTO_SERVER -m "$status"
 		else 
 			if [ "$absent_counter" < "$absent_send" ]; then
-				absent_counter = $(( $absent_counter + 1 ))
+				absent_counter=$(( $absent_counter + 1 ))
 				echo "incrementing absent_counter"
 			else 
 				echo "absent counter reached"
-				absent_counter = 0
+				absent_counter=0
 				echo "sending absent"
 			fi
 
